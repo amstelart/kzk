fa7635db593fdc5d826974852576ee54a964922d
Артем <amstelart@MacBook-Pro-Artem.local>
2018-01-22 14:58:39 +0300

    add main sections

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,7 +1,69 @@
 // Если на проекте jQuery
-// $( document ).ready(function() {
-//   // code
-// });
+$( document ).ready(function() {
+  // code
+  $("#main-carousel-01").owlCarousel({
+    items: 2,
+    nav: false,
+    dots: true,
+    loop: true,
+    margin: 10,
+    center: false,
+    responsive : {
+      0 : {
+        items: 1,
+        nav: true,
+        loop: true,
+        center: true,
+      },
+      480 : {
+        items: 2,
+      },
+      768 : {
+        items: 2,
+      },
+      992 : {
+        items: 2,
+      },
+      1200 : {
+        items: 2,
+      },
+      1800 : {
+        items: 2,
+      }
+    }
+  });
+  $("#main-carousel-02").owlCarousel({
+    items: 2,
+    nav: false,
+    dots: true,
+    loop: true,
+    margin: 30,
+    center: false,
+    responsive : {
+      0 : {
+        items: 1,
+        nav: true,
+        loop: true,
+        center: true,
+      },
+      480 : {
+        items: 2,
+      },
+      768 : {
+        items: 2,
+      },
+      992 : {
+        items: 4,
+      },
+      1200 : {
+        items: 4,
+      },
+      1800 : {
+        items: 4,
+      }
+    }
+  });
+});
 
 // Изоляция без jQuery
 // (function(){
