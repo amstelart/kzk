c663016c4fb608bb644a752fd0d01af30d230d38
amstelart <amsteldroid@gmail.com>
2019-03-05 12:07:34 +0300

    Update 2019-03-05T09:07:32.763Z

--- js/global-script.js
+++ js/global-script.js
@@ -180,21 +180,3 @@ $(document).ready(function() {
     });
 
 });
-
-// Изоляция без jQuery
-// (function(){
-//   // code
-// }());
-
-// На проекте нет jQuery, но хочется $( document ).ready...
-// function ready(fn) {
-//   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
-//     fn();
-//   } else {
-//     document.addEventListener('DOMContentLoaded', fn);
-//   }
-// }
-//
-// ready(function(){
-//   // code
-// });
